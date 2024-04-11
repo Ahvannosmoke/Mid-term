81b33036888beca3aa547532a626ba27448b78c7
Sovann Sam <samsovann2003@gmail.com>
2024-04-11 02:48:53 -0700

    last

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/vann/ws2/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/vann/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/vann/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/two_links/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vann/ws2/src/two_links
+# Build directory: /home/vann/ws2/build/two_links
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
