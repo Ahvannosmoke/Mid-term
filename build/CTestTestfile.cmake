5e2bf244b3a976ed759d64d37f20c4f4377f7c15
Sovann Sam <samsovann2003@gmail.com>
2024-04-11 02:50:01 -0700

    remove junks

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vann/ws2/src
-# Build directory: /home/vann/ws2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("two_links")
